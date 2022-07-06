cc71fcfd761d7d3ceb665b2b1989fa8d9e7f9e7f
Azad ul kabir <azadulkabir10@gmail.com>
2022-07-06 09:22:27 +0600

    Project initialize with webpack

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.scss"
+
+export default function App() {
+  return (
+   <>
+<ul className="nav nav-pills">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Link</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Link</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+   </>
+)
+}
