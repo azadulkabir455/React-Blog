41ed029fc5b08c007e3bff26537cb796d52ef823
Azad ul kabir <azadulkabir10@gmail.com>
2022-07-19 09:17:34 +0600

    Context and Json Server added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import "./style.scss"
+import "./Resources/Css/global.scss"
 
 export default function App() {
   return (
    <>
-<ul className="nav nav-pills">
-  <li className="nav-item">
-    <a className="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link disabled">Disabled</a>
-  </li>
-</ul>
    </>
 )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM  from "react-dom";
+import {GlobalDataContainer} from "./Context/Context";
+import { BrowserRouter as Routers } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+<>
+    <React.StrictMode>
+        <GlobalDataContainer>
+            <Routers>
+                <App />
+            </Routers>
+        </GlobalDataContainer>
+    </React.StrictMode>
+</>, 
+
+document.querySelector("#root"));
